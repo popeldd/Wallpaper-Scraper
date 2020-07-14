5d115dc1c96d46f51aefd256c0183fcadbfc0370
popeldd <daniladpopel@gmail.com>
2020-07-14 19:58:41 -0400

    wallscraper.py

--- wallscraper.py
+++ wallscraper.py
@@ -1,76 +1,86 @@
 import shutil, random, os, platform, urllib.request, time, ctypes
 from bs4 import BeautifulSoup as bs
 
-# --- for user input
+# ---
 
 url = urllib.request.urlopen('https://www.unsplash.com') # main url
-url_imageserver = 'https://images.unsplash.com/photo-' # image server url
+url_imagenameserver = 'https://images.unsplash.com/photo-' # image server url
 
 # ---
 
-def download_image(i,temp_path):
-    image_name = 'image-{}.jpg'.format(image_count)
-    full_path = '{}\{}'.format(temp_path,image_name)
-    urllib.request.urlretrieve(i, full_path)
-    print('Item {} has successfully saved as {}.'.format(n+1,image_name))
+def check_temp_path():
+    while os.path.isdir(temp_path):
+        remove_temp_path()
+def make_temp_path():
+    global temp_path, temp_path_full
+    if user_platform == 'windows':
+        temp_path = os.getcwd() + r'\temp'
+        temp_path_full = '{}\{}'
+        check_temp_path()
+        os.mkdir(temp_path)
+    elif user_platform == 'linux':
+        temp_path = os.getcwd() + '/temp'
+        temp_path_full = '{}/{}'
+        check_temp_path()
+        os.mkdir(temp_path)
+    elif user_platform == 'debian':
+        return
+    else:
+        print('Error: Unknown platform: {}'.format(user_platform))
 
 def remove_temp_path():
     try:
         shutil.rmtree(temp_path)
     except OSError as e:
-        print("Error: {}".format(e.strerror))
+        print('Error: {}'.format(e.strerror))
+
+def download_image(i,temp_path):
+    image_name = 'image-{}.jpg'.format(image_count)
+    full_path = temp_path_full.format(temp_path,image_name)
+    urllib.request.urlretrieve(i, full_path)
+    print('Item {} has successfully saved as {}.'.format(n+1,image_name))
 
 def set_wallpaper(x):
-    if user_platform == 'Windows': #windows support
+    if user_platform == 'windows':
         image_path = temp_path + r'\image-{}.jpg'.format(x)
         ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 0)
-        print('Windows: Wallpaper set to image-{}.jpg.'.format(x))
-    elif user_platform == 'Linux': #gnome gui support: I will fix this soon
-        os.system('/usr/bin/gsettings set org.gnome.desktop.background picture-uri' + image_path)
-    elif user_platform == 'Darwin': #mac support: I will fix this soon
+        time.sleep(1)
+        remove_temp_path()
+    elif user_platform == 'linux':
+        image_path = temp_path + '/image-{}.jpg'.format(x)
+        os.system('gsettings set org.gnome.desktop.background picture-uri {}'.format(image_path))
+    elif user_platform == 'darwin':
         return
     else:
-        print("Error: Unknown platform: {}".format(user_platform))
-    time.sleep(1)
-
-
+        print('Error: Unknown platform: {}'.format(user_platform))
+    print('{}: Wallpaper set to image-{}.jpg.'.format(user_platform.capitalize(), x))
 
 # ---
 
-user_platform = platform.system()
-temp_path = os.getcwd() + r'\temp'
-while os.path.isdir(temp_path):
-    remove_temp_path()
+if __name__ == '__main__':
+    user_platform = platform.system().lower()
+    make_temp_path()
 
-os.mkdir(temp_path)
+    image_urls = []
+    for i in bs(url, 'lxml').findAll('img'):
+        image_urls.append(i.get('src').split('&w=',1)[0])
 
-"""
-for i in bs(url, 'lxml').findAll('source'):
-    image_urls.append(i.get('srcset').split('?',1)[0])
-for i in bs(url, 'lxml').findAll('img'):
-    image_urls.append(i.get('src').split('&w=',1)[0])
-"""
-image_count = 0
-image_urls = []
-for i in bs(url, 'lxml').find_all('img'):
-    image_urls.append(i.get('src').split('&w=',1)[0])
+    try:
+        image_count = 0
+        for n,i in enumerate(image_urls):
+            if url_imagenameserver in i:
+                image_count += 1
+                download_image(i,temp_path)
+            else:
+                print('Item {} did not match url_imagenameserver specifications.'.format(n+1))
+            print(i,end='\n\n')
 
-try:
-    for n,i in enumerate(image_urls):
-        if url_imageserver in i:
-            image_count += 1
-            download_image(i,temp_path)
-        else:
-            print('Item {} did not match url_imageserver specifications.'.format(n+1))
-        print(i,end='\n\n')
-
-except OSError as e:
-    print("Error: {}".format(e.strerror))
-    time.sleep(1)
+    except OSError as e:
+        print("Error: {}".format(e.strerror))
+        time.sleep(1)
 
 # ---
 
-set_wallpaper(random.randint(1,image_count))
-remove_temp_path()
+    set_wallpaper(random.randint(1,image_count))
 
 # ---
